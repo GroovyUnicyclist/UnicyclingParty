9b4f31bde9b5612cac46d1d324464ad4dde23f64
Nick Braun <nwbx13@gmail.com>
2020-09-09 02:22:17 -0400

    Update Discord stuff

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::domain('osu.'.env('APP_URL'))->group( function() {
 
 Route::domain('discord.'.env('APP_URL'))->group( function() {
     Route::get('/', 'HomeController@discord')->name('discord');
+
+    Route::redirect('/invite', 'https://discord.gg/8BAb7Cf')->name('invite');
 });
 
 Auth::routes(['register' => false]);
